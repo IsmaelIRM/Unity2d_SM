3c3bd7b2b7a7aea22597a185a9f2a1dab91b5d72
AEgurenD <aeguren@alumnos.unex.es>
2026-03-17 18:28:26 +0100

    Animacion movimiento player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,41 +1,54 @@
 using UnityEngine;
+
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Movimiento")]
     public float moveSpeed = 7f;
     public float jumpForce = 14f;
+
     [Header("Detección de suelo")]
     public Transform groundCheck;
     public float groundCheckRadius = 0.2f;
     public LayerMask groundLayer;
+
     private Rigidbody2D rb;
     private SpriteRenderer sr;
+    private Animator anim; 
+
     private bool isGrounded;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>(); 
     }
+
     void Update()
     {
-        // Comprobar si está en el suelo
-        isGrounded = Physics2D.OverlapCircle(
-        groundCheck.position,
-        groundCheckRadius,
-        groundLayer);
-        // Saltar
+        // 1. Comprueba si tocamos el suelo
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        // 2. SALTO: "Jump" en Unity es la barra espaciadora por defecto
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
         }
-        // Voltear sprite según dirección
+
+        // 3. MOVIMIENTO: "Horizontal" lee automáticamente la A (-1) y la D (1)
         float h = Input.GetAxisRaw("Horizontal");
+        
+        // Volteamos el sprite según la dirección
         if (h > 0) sr.flipX = false;
         else if (h < 0) sr.flipX = true;
+
+        // Le pasamos la velocidad al Animator para que cambie de Idle a Run
+        anim.SetFloat("Speed", Mathf.Abs(h));
     }
+
     void FixedUpdate()
     {
-        // Movimiento horizontal
+        // Aplicamos la fuerza física real basada en si pulsaste A o D
         float h = Input.GetAxisRaw("Horizontal");
         rb.linearVelocity = new Vector2(h * moveSpeed, rb.linearVelocity.y);
     }
